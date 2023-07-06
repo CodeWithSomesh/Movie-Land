07345c52f1f4821ebf69bc526735584779c32e57
CodeWithSomesh <codewithsomesh@gmail.com>
2023-07-06 21:54:13 +0800

    Fetching API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import {useEffect} from "react";
 import './index.css';
+
 
 
 export default function App () {
 
+  
+    const API_url = "https://www.omdbapi.com?apikey=cb0c7f0f";
+
+    useEffect(() => {
+
+      const fetchMovies = async () => {
+        const res = await fetch(API_url);
+        const data = await res.json();
+      }
+
+    })
+
+
   return (
     <main>
-      Body is sexy
+      Body 
     </main>
   )
 
